15e5a2c3f4b6bae7c3aa715215464cc2ec73529b
gamartins1 <gabriel.550@hotmail.com>
2024-04-10 20:42:20 -0300

    Correção: Aumento no tamanho máximo das colunas do JWT e da causa do erro na entidade

--- src/main/java/com/gabriel/jwt/model/CallRecord.java
+++ src/main/java/com/gabriel/jwt/model/CallRecord.java
@@ -3,13 +3,13 @@ package com.gabriel.jwt.model;
 import static jakarta.persistence.GenerationType.IDENTITY;
 import static org.apache.logging.log4j.util.Strings.EMPTY;
 
-import java.sql.Date;
 import java.time.LocalDateTime;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
+import jakarta.persistence.Lob;
 import jakarta.persistence.Table;
 
 @Entity
@@ -20,8 +20,10 @@ public class CallRecord {
     @GeneratedValue(strategy = IDENTITY)
     @Column(name = "Id_Record")
     private Long id;
+    @Lob
     private String jwt;
     private Boolean isValid;
+    @Lob
     private String cause;
     @Column(name = "Record_Date")
     private LocalDateTime recordDateTime;
